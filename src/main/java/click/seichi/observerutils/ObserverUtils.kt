29f2f648ada4b6ddb459f5fbe84a9915db7fe82f
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-14 21:09:04 +0900

    feat(Main): add companion object of JavaPlugin

--- src/main/java/click/seichi/observerutils/ObserverUtils.kt
+++ src/main/java/click/seichi/observerutils/ObserverUtils.kt
@@ -1,9 +1,16 @@
 package click.seichi.observerutils
 
+import click.seichi.observerutils.commands.Command
 import org.bukkit.plugin.java.JavaPlugin
 
 class ObserverUtils : JavaPlugin() {
+    companion object {
+        lateinit var PLUGIN: ObserverUtils
+            private set
+    }
+
     override fun onEnable() {
+        PLUGIN = this
         getCommand("obs").executor = Command
     }
 
