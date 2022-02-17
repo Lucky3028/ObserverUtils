dbb450c7062393cf5def7d0d54357d5fc81b03e1
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-17 21:11:35 +0900

    Revert "chore: not extended function but method"
    asTabExecutorがoverrideできてしまう
    This reverts commit e46b2abb83fcd511e9b172dc24500a9490c23fce.

--- src/main/java/click/seichi/observerutils/contextualexecutor/ContextualExecutor.kt
+++ src/main/java/click/seichi/observerutils/contextualexecutor/ContextualExecutor.kt
@@ -10,32 +10,32 @@ interface ContextualExecutor {
     fun executeWith(context: RawCommandContext): Result<Any, Throwable>
 
     fun tabCandidatesFor(context: RawCommandContext): List<String> = emptyList()
+}
 
-    fun asTabExecutor() = object: TabExecutor {
-        override fun onTabComplete(
-            sender: CommandSender,
-            command: Command,
-            alias: String,
-            args: Array<out String>
-        ): MutableList<String> {
-            val context = RawCommandContext(sender, ExecutedCommand(command, alias), args.toList())
+fun ContextualExecutor.asTabExecutor() = object: TabExecutor {
+    override fun onTabComplete(
+        sender: CommandSender,
+        command: Command,
+        alias: String,
+        args: Array<out String>
+    ): MutableList<String> {
+        val context = RawCommandContext(sender, ExecutedCommand(command, alias), args.toList())
 
-            return tabCandidatesFor(context).toMutableList()
+        return tabCandidatesFor(context).toMutableList()
+    }
+
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>
+    ): Boolean {
+        val context = RawCommandContext(sender, ExecutedCommand(command, label), args.toList())
+        executeWith(context).onFailure {
+            println("Error has occured while executing ${command.name} command.")
+            it.printStackTrace()
         }
 
-        override fun onCommand(
-            sender: CommandSender,
-            command: Command,
-            label: String,
-            args: Array<out String>
-        ): Boolean {
-            val context = RawCommandContext(sender, ExecutedCommand(command, label), args.toList())
-            executeWith(context).onFailure {
-                println("Error has occured while executing ${command.name} command.")
-                it.printStackTrace()
-            }
-
-            return true
-        }
+        return true
     }
 }
