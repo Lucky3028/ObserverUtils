ee17cc41f09d5b3ad1af15b936b3b241e651e20e
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-21 23:47:01 +0900

    feat: add PartiallyParsedArgs and ParsedArgCommandContext

--- src/main/java/click/seichi/observerutils/contextualexecutor/Contexts.kt
+++ src/main/java/click/seichi/observerutils/contextualexecutor/Contexts.kt
@@ -12,3 +12,14 @@ data class RawCommandContext(
     val command: ExecutedCommand,
     val args: List<String>
 )
+
+data class PartiallyParsedArgs(
+    val parsed: List<Any>,
+    val yetToBeParsed: List<String>
+)
+
+data class ParsedArgCommandContext<out CS: CommandSender>(
+    val sender: CS,
+    val command: ExecutedCommand,
+    val args: PartiallyParsedArgs
+)
