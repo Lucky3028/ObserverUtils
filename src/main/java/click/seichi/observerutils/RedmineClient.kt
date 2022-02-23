f33bc0ba28515647ae34dffab690f7bad42dc3e7
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-23 11:52:19 +0900

    feat: add RedmineClient to create issue in Redmine

---
+++ src/main/java/click/seichi/observerutils/RedmineClient.kt
@@ -0,0 +1,19 @@
+package click.seichi.observerutils
+
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.fuel.core.extensions.jsonBody
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class RedmineClient(private val redmineApiKey: String) {
+    private val redmineDomain = "https://redmine.seichi.click"
+    private val redmineIssueUrl = "$redmineDomain/issues.json?key=$redmineApiKey"
+
+    fun postIssue(issue: RedmineIssue): Pair<Request, Response> {
+        val content = Json.encodeToString(Issue(issue))
+        val res = Fuel.post(redmineIssueUrl).jsonBody(content).header("User-Agent", "curl/7.38.0").response()
+        return res.first to res.second
+    }
+}
