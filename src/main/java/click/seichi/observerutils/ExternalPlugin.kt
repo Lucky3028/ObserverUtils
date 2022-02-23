4156503741133ff5606738be23d63f96df4ddb34
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-23 11:53:14 +0900

    feat: add ExternalPlugin object to get external plugin instance

---
+++ src/main/java/click/seichi/observerutils/ExternalPlugin.kt
@@ -0,0 +1,21 @@
+package click.seichi.observerutils
+
+import arrow.core.None
+import arrow.core.getOrElse
+import arrow.core.toOption
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.World
+
+object ExternalPlugin {
+    object WorldGuard {
+        private val instance = Bukkit.getPluginManager().getPlugin("WorldGuard").toOption()
+            .map { it as WorldGuardPlugin }.getOrElse { throw Exception("WorldGuard cannnot be found!") }
+        private fun regionManager(world: World) = instance.regionContainer.get(world).toOption()
+
+        fun getRegions(world: World, location: Location) =
+            regionManager(world).map { it.getApplicableRegions(location).filterNotNull().toSet() }
+                .flatMap { if (it.isEmpty()) None else it.toOption() }
+    }
+}
