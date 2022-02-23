6e32699b44a2ff62822636cd1e39dbea607f4f66
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-23 11:52:45 +0900

    feat: add Logger class wrapping Bukkit Logger

---
+++ src/main/java/click/seichi/observerutils/Logger.kt
@@ -0,0 +1,25 @@
+package click.seichi.observerutils
+
+import click.seichi.observerutils.LoggerLevel.*
+import org.bukkit.Bukkit
+// TODO: Throwable Eitherをまとめる
+enum class LoggerLevel{
+    INFO, WARN, SEVERE
+}
+
+object Logger {
+    private val logger = Bukkit.getServer().logger
+
+    fun log(messages: List<String>, level: LoggerLevel = INFO) = when(level){
+        INFO -> info(messages)
+        WARN -> warn(messages)
+        SEVERE -> severe(messages)
+    }
+
+    fun info(message: String) = info(listOf(message))
+    fun info(messages: List<String>) = messages.forEach { logger.info(it) }
+    fun warn(message: String) = warn(listOf(message))
+    fun warn(messages: List<String>) = messages.forEach { logger.warning(it) }
+    fun severe(message: String) = severe(listOf(message))
+    fun severe(messages: List<String>) = messages.forEach { logger.severe(it) }
+}
