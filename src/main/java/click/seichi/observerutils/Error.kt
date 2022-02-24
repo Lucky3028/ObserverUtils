6450d9cd3bb78a9cff549349611f1050441de233
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-25 00:34:45 +0900

    fix: make WrappedException#exception public

--- src/main/java/click/seichi/observerutils/Error.kt
+++ src/main/java/click/seichi/observerutils/Error.kt
@@ -6,7 +6,7 @@ sealed interface Error {
     val error: String
 }
 
-class WrappedException(private val exception: Exception) : Error {
+class WrappedException(val exception: Exception) : Error {
     fun printStackTrace() = exception.printStackTrace()
     fun stackTrace() = exception.stackTraceToString()
 
