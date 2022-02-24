52f5254d9e3f9a4226bffeadee84de9984b92ae5
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-25 00:33:59 +0900

    feat: add HttpExeption

--- src/main/java/click/seichi/observerutils/Error.kt
+++ src/main/java/click/seichi/observerutils/Error.kt
@@ -20,3 +20,31 @@ sealed interface CommandBuildException : Error {
         override val error = "引数が足りません。: $missingArg"
     }
 }
+
+abstract class HttpException : Error {
+    companion object: SealedClassEnumExtension<HttpException>
+
+    object Unauthorized : HttpException() {
+        override val statusCode = 401
+        override val error = "認証に失敗しました。APIキーを確認してください。"
+    }
+
+    object Forbidden : HttpException() {
+        override val statusCode = 403
+        override val error = "コンテンツへのアクセスが拒否されました。権限設定を確認してください。"
+    }
+
+    object NotFound : HttpException() {
+        override val statusCode = 404
+        override val error = "指定されたURLは存在しません。"
+    }
+
+    object UnProcessableEntity : HttpException() {
+        override val statusCode = 422
+        override val error = "エンティティを処理できませんでした。Bodyを確認してください。"
+    }
+
+    class Unknown(override val statusCode: Int, override val error: String) : HttpException()
+
+    abstract val statusCode: Int
+}
