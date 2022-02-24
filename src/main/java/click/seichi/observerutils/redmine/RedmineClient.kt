53acde97710e784491680d03cc122e37a7aeafb1
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-25 00:35:45 +0900

    fix: RedmineClient#postIssueの返り値をEitherに

--- src/main/java/click/seichi/observerutils/redmine/RedmineClient.kt
+++ src/main/java/click/seichi/observerutils/redmine/RedmineClient.kt
@@ -1,19 +1,25 @@
 package click.seichi.observerutils.redmine
 
+import arrow.core.Either
+import click.seichi.observerutils.HttpException
 import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.fuel.core.Response
 import com.github.kittinunf.fuel.core.extensions.jsonBody
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import values
 
 class RedmineClient(redmineApiKey: String) {
     private val redmineDomain = "https://redmine.seichi.click"
     private val redmineIssueUrl = "$redmineDomain/issues.json?key=$redmineApiKey"
 
-    fun postIssue(issue: RedmineIssue): Pair<Request, Response> {
+    fun postIssue(issue: RedmineIssue): Either<HttpException, Response> {
         val content = Json.encodeToString(Issue(issue))
         val res = Fuel.post(redmineIssueUrl).jsonBody(content).header("User-Agent", "curl/7.38.0").response()
-        return res.first to res.second
+        val statusCode = res.second.statusCode
+        return if (statusCode in 200..399) Either.Right(res.second)
+        else Either.Left(
+            HttpException.values().find { it.statusCode == statusCode } ?: HttpException.Unknown(statusCode, res.second.responseMessage)
+        )
     }
 }
