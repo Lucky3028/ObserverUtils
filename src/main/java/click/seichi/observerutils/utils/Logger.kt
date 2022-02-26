dd6cee261c10c05502517aae7bda067aa4ae0064
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-26 21:16:45 +0900

    docs: add info about Logger

--- src/main/java/click/seichi/observerutils/utils/Logger.kt
+++ src/main/java/click/seichi/observerutils/utils/Logger.kt
@@ -16,13 +16,27 @@ enum class LoggerLevel {
 object Logger {
     private val logger = Bukkit.getServer().logger
 
+    /**
+     * [LoggerLevel]で[messages]をサーバーログに出力する
+     * @param level ロギングレベル。デフォルトは[LoggerLevel.INFO]。
+     * param messages 出力したいメッセージ。
+     */
     fun log(level: LoggerLevel = INFO, vararg messages: String) = when (level) {
         INFO -> info(*messages)
         WARN -> warn(*messages)
         SEVERE -> severe(*messages)
     }
 
+    /**
+     * [log]を[LoggerLevel.INFO]で実行するシンタックスシュガー。
+     */
     fun info(vararg messages: String) = messages.forEach { logger.info(it) }
+    /**
+     * [log]を[LoggerLevel.WARN]で実行するシンタックスシュガー。
+     */
     fun warn(vararg messages: String) = messages.forEach { logger.warning(it) }
+    /**
+     * [log]を[LoggerLevel.SEVERE]で実行するシンタックスシュガー。
+     */
     fun severe(vararg messages: String) = messages.forEach { logger.severe(it) }
 }
