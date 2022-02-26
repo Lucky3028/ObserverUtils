9143f86abfe0d37ae3079d9cd7aef28260000362
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-26 20:56:35 +0900

    docs: add docs to extensions

--- src/main/java/click/seichi/observerutils/utils/Extension.kt
+++ src/main/java/click/seichi/observerutils/utils/Extension.kt
@@ -3,8 +3,23 @@ package click.seichi.observerutils.utils
 import arrow.core.Option
 import org.bukkit.Location
 
+/**
+ * [org.bukkit.Location]を文字列にフォーマットする
+ * @return X、Y、Z座標を`0 0 0`という順にフォーマットした[String]
+ */
 fun Location.formatted() = "${this.blockX} ${this.blockY} ${this.blockZ}"
 
+/**
+ * 与えられた[Collection]の先頭のアイテムと残りのアイテムのリストを返す
+ * @return 与えられた[Collection]の先頭のアイテムと残りのアイテムの[List]を[Pair]で[arrow.core.Some]に包んで返す。[Collection]が空ならば、[arrow.core.None]を返す。
+ */
 fun <T> Collection<T>.splitFirst() = Option.catch { this.first() to this.drop(1) }
+
+/**
+ * 与えられた[Collection]が空ならば[default]、からでなければ[formatter]で指定された関数を適用して返す
+ * @param default [Collection]が空の時に返る[String]
+ * @param formatter [Collection]が空ではない時に適用する関数
+ * @return [String]
+ */
 fun <T> Collection<T>.orEmpty(default: String, formatter: (Collection<T>) -> String) =
     if (this.isEmpty()) default else formatter(this)
