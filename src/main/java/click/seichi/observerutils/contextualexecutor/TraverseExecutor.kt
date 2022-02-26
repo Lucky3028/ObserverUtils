943914d8f1f5cb55fa842e0a46f63b78e988dfdb
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-26 23:04:52 +0900

    feat: add TraverseExecutor

---
+++ src/main/java/click/seichi/observerutils/contextualexecutor/TraverseExecutor.kt
@@ -0,0 +1,16 @@
+package click.seichi.observerutils.contextualexecutor
+
+import arrow.core.Either
+import click.seichi.observerutils.EffectOrErr
+
+/**
+ * [executors]を実行するのExecutor
+ * @param executors 実行する[ContextualExecutor]。
+ */
+class TraverseExecutor(private vararg val executors: ContextualExecutor): ContextualExecutor {
+    override suspend fun executeWith(context: RawCommandContext): EffectOrErr {
+        executors.forEach { it.executeWith(context) }
+
+        return Either.Right(Effect.EmptyEffect)
+    }
+}
