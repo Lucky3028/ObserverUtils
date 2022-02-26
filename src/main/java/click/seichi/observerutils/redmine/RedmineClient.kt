827a5055f9dacde653bccdfd1fafbbc7fd54627e
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-26 16:26:47 +0900

    fix: RedmineIssueのドメインをgsonへの変更に対応させる

--- src/main/java/click/seichi/observerutils/redmine/RedmineClient.kt
+++ src/main/java/click/seichi/observerutils/redmine/RedmineClient.kt
@@ -2,24 +2,26 @@ package click.seichi.observerutils.redmine
 
 import arrow.core.Either
 import click.seichi.observerutils.HttpException
+import click.seichi.observerutils.KnownHttpException
+import click.seichi.observerutils.UnknownHttpException
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.Response
 import com.github.kittinunf.fuel.core.extensions.jsonBody
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import values
+import com.google.gson.Gson
 
 class RedmineClient(redmineApiKey: String) {
     private val redmineDomain = "https://redmine.seichi.click"
     private val redmineIssueUrl = "$redmineDomain/issues.json?key=$redmineApiKey"
 
-    fun postIssue(issue: RedmineIssue): Either<HttpException, Response> {
-        val content = Json.encodeToString(Issue(issue))
+    fun postIssue(issue: RedmineIssue): Either<Pair<HttpException, String>, Response> {
+        val content = Gson().toJson(Issue(issue))
         val res = Fuel.post(redmineIssueUrl).jsonBody(content).header("User-Agent", "curl/7.38.0").response()
         val statusCode = res.second.statusCode
         return if (statusCode in 200..399) Either.Right(res.second)
-        else Either.Left(
-            HttpException.values().find { it.statusCode == statusCode } ?: HttpException.Unknown(statusCode, res.second.responseMessage)
-        )
+        else {
+            val err = KnownHttpException.values().find { it.statusCode == statusCode }
+                ?: UnknownHttpException(statusCode, res.second.responseMessage)
+            Either.Left(err to content)
+        }
     }
 }
--- src/main/java/click/seichi/observerutils/redmine/RedmineIssue.kt
+++ src/main/java/click/seichi/observerutils/redmine/RedmineIssue.kt
@@ -1,22 +1,14 @@
 package click.seichi.observerutils.redmine
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
 const val IssueNewStatusId = 1
 const val ObsProjectId = 4
 
-@Serializable
 class Issue(private val issue: RedmineIssue)
 
-@Serializable
 class RedmineIssue private constructor(
-    @SerialName("project_id")
-    val projectId: Int,
-    @SerialName("tracker_id")
-    val trackerId: Int,
-    @SerialName("status_id")
-    val statusId: Int,
+    val projectd: Int,
+    val tracker_id: Int,
+    val status_id: Int,
     val subject: String,
     val description: String
 ) {
