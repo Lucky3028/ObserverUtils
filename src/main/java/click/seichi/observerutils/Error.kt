446f301c043955abcce029456c320c1438c31a36
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-26 16:28:56 +0900

    refactor: HttpExeptionをenumに

--- src/main/java/click/seichi/observerutils/Error.kt
+++ src/main/java/click/seichi/observerutils/Error.kt
@@ -21,30 +21,15 @@ sealed interface CommandBuildException : Error {
     }
 }
 
-abstract class HttpException : Error {
-    companion object: SealedClassEnumExtension<HttpException>
-
-    object Unauthorized : HttpException() {
-        override val statusCode = 401
-        override val error = "認証に失敗しました。APIキーを確認してください。"
-    }
-
-    object Forbidden : HttpException() {
-        override val statusCode = 403
-        override val error = "コンテンツへのアクセスが拒否されました。権限設定を確認してください。"
-    }
-
-    object NotFound : HttpException() {
-        override val statusCode = 404
-        override val error = "指定されたURLは存在しません。"
-    }
-
-    object UnProcessableEntity : HttpException() {
-        override val statusCode = 422
-        override val error = "エンティティを処理できませんでした。Bodyを確認してください。"
-    }
-
-    class Unknown(override val statusCode: Int, override val error: String) : HttpException()
-
-    abstract val statusCode: Int
+sealed interface HttpException : Error {
+    val statusCode: Int
 }
+
+enum class KnownHttpException(override val statusCode: Int, override val error: String) : HttpException {
+    Unauthorized(401, "認証に失敗しました。APIキーを確認してください。"),
+    Forbidden(403, "コンテンツへのアクセスが拒否されました。権限設定を確認してください。"),
+    NotFound(404, "指定されたURLは存在しません。"),
+    UnProcessableEntity(422, "エンティティを処理できませんでした。Bodyを確認してください。");
+}
+
+data class UnknownHttpException(override val statusCode: Int, override val error: String) : HttpException
