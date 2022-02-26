c849c8a2817f8018646a5aadbd250e3d4be2bd51
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-26 16:31:42 +0900

    style: add new line

--- src/main/java/click/seichi/observerutils/utils/Extension.kt
+++ src/main/java/click/seichi/observerutils/utils/Extension.kt
@@ -6,5 +6,5 @@ import org.bukkit.Location
 fun Location.formatted() = "${this.blockX} ${this.blockY} ${this.blockZ}"
 
 fun <T> Collection<T>.splitFirst() = Option.catch { this.first() to this.drop(1) }
-fun <T> Collection<T>.orEmpty(default: String) = if (this.isEmpty()) default else this.toString()
-fun <T> Collection<T>.orEmpty(default: String, formatter: (Collection<T>) -> String) = if (this.isEmpty()) default else formatter(this)
+fun <T> Collection<T>.orEmpty(default: String, formatter: (Collection<T>) -> String) =
+    if (this.isEmpty()) default else formatter(this)
