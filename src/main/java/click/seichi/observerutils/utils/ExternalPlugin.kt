e092ea67a347358bf248f1c3d59ab3de2a8422c0
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-26 21:04:35 +0900

    docs: add docs to external plugins

--- src/main/java/click/seichi/observerutils/utils/ExternalPlugin.kt
+++ src/main/java/click/seichi/observerutils/utils/ExternalPlugin.kt
@@ -12,6 +12,9 @@ import org.bukkit.Location
 import org.bukkit.World
 import org.bukkit.entity.Player
 
+/**
+ * 外部プラグインを扱うためのobject。
+ */
 object ExternalPlugin {
     object WorldGuard {
         private val instance = Bukkit.getPluginManager().getPlugin("WorldGuard").toOption()
@@ -19,6 +22,12 @@ object ExternalPlugin {
 
         private fun regionManager(world: World) = instance.regionContainer.get(world).toOption()
 
+        /**
+         * 指定された[World]の[Location]に存在するWorldGuardの保護を返す
+         * @param world 保護を取得したい[World]
+         * @param location 保護を取得したい[Location]
+         * @return 指定された[World]の[Location]に存在するWorldGuardの保護の[Set]。保護がなければ[None]。
+         */
         fun getRegions(world: World, location: Location) =
             regionManager(world).map { it.getApplicableRegions(location).filterNotNull().toSet() }
                 .flatMap { if (it.isEmpty()) None else it.toOption() }
@@ -28,8 +37,16 @@ object ExternalPlugin {
         private val instance = Bukkit.getPluginManager().getPlugin("WorldEdit").toOption()
             .map { it as WorldEditPlugin }.getOrElse { throw Exception("WorldEdit cannot be found!") }
 
+        /**
+         * WorldEditの[com.sk89q.worldedit.bukkit.selections.Selection]をラップするdata class。
+         */
         data class Selection(val min: Location, val max: Location)
 
+        /**
+         * 指定された[Player]がWorldEditで選択している範囲の最小座標と最大座標を[Location]で返す
+         * @param p 選択範囲を取得したい[Player]
+         * @return 指定された[Player]がWorldEditで選択している範囲の最小座標と最大座標を示す[Location]を持つ[Selection]。両方選択されていなければ[None]。
+         */
         suspend fun getSelections(p: Player): Option<Selection> = option {
             val selection = instance.getSelection(p).toOption().bind()
             val min = selection.minimumPoint.toOption().bind()
