3780b6df4568e19045b313478204b12f20108f16
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-09 13:59:08 +0900

    feat: add world enum

---
+++ src/main/kotlin/click/seichi/observerutils/redmine/World.kt
@@ -0,0 +1,13 @@
+package click.seichi.observerutils.redmine
+
+enum class World(val worldName: String, val ja: String) {
+    Main("world_2", "メイン"),
+    Seichi1("world_SW", "第1整地"),
+    Seichi2("world_SW_2", "第2整地"),
+    Seichi3("world_SW_3", "第3整地"),
+    Seichi4("world_SW_4", "第4整地"),
+    SeichiNether("world_SW_nether", "ネザー整地"),
+    SeichiEnd("world_SW_the_end", "エンド整地"),
+    Build("world_build", "建築専用"),
+    Dot("world_dot", "ドット絵")
+}
