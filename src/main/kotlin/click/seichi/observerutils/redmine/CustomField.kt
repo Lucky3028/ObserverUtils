446e3568ce0ef2fa97be55b02f874d498f9e6ec3
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-10 20:15:20 +0900

    feat: add enum class for CustomField

---
+++ src/main/kotlin/click/seichi/observerutils/redmine/CustomField.kt
@@ -0,0 +1,10 @@
+package click.seichi.observerutils.redmine
+
+enum class CustomField(val id: Int) {
+    Server(11),
+    World(12),
+    Location(13),
+    Location2(14),
+    Reason(19),
+    Content(20)
+}
