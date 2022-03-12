b3b60fbda0abce5b3867157658dc0f6d1df38a37
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-12 21:00:21 +0900

    feat: add command args parsers

---
+++ src/main/kotlin/click/seichi/observerutils/contextualexecutor/Parsers.kt
@@ -0,0 +1,23 @@
+package click.seichi.observerutils.contextualexecutor
+
+import click.seichi.observerutils.CommandBuildException
+import com.github.michaelbull.result.get
+import com.github.michaelbull.result.mapResult
+import com.github.michaelbull.result.runCatching
+import com.github.michaelbull.result.toResultOr
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+object Parsers {
+    fun formattedDate(vararg formatter: DateTimeFormatter, failureMessage: String): SingleArgumentParser = { str ->
+        formatter.firstNotNullOfOrNull { runCatching { LocalDate.parse(str, it) }.get() }
+            .toResultOr { CommandBuildException.ParseArgument(failureMessage) }
+    }
+
+    fun listedInt(list: List<Int>, failureMessage: String): SingleArgumentParser = { str ->
+        str.split(",").mapResult { parsedList ->
+            parsedList.toIntOrNull()?.takeIf { list.contains(it) }
+                .toResultOr { CommandBuildException.ParseArgument(failureMessage) }
+        }
+    }
+}
