6e9106da7160589ee0d2980a6349e6b31d28cc60
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-03-12 21:04:20 +0900

    chore: rename CommandBuildException.ParseArgument -> FailedToParseArg

--- src/main/kotlin/click/seichi/observerutils/contextualexecutor/Parsers.kt
+++ src/main/kotlin/click/seichi/observerutils/contextualexecutor/Parsers.kt
@@ -11,13 +11,13 @@ import java.time.format.DateTimeFormatter
 object Parsers {
     fun formattedDate(vararg formatter: DateTimeFormatter, failureMessage: String): SingleArgumentParser = { str ->
         formatter.firstNotNullOfOrNull { runCatching { LocalDate.parse(str, it) }.get() }
-            .toResultOr { CommandBuildException.ParseArgument(failureMessage) }
+            .toResultOr { CommandBuildException.FailedToParseArg(failureMessage) }
     }
 
     fun listedInt(list: List<Int>, failureMessage: String): SingleArgumentParser = { str ->
         str.split(",").mapResult { parsedList ->
             parsedList.toIntOrNull()?.takeIf { list.contains(it) }
-                .toResultOr { CommandBuildException.ParseArgument(failureMessage) }
+                .toResultOr { CommandBuildException.FailedToParseArg(failureMessage) }
         }
     }
 }
