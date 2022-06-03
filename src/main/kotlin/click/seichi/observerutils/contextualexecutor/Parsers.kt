8371fd77297d6cac4edb6bff88fccf6198c49737
Lucky3028 <lucky3028@users.noreply.github.com>
2022-06-03 21:12:56 +0900

    refactor: remove unused parser

--- src/main/kotlin/click/seichi/observerutils/contextualexecutor/Parsers.kt
+++ src/main/kotlin/click/seichi/observerutils/contextualexecutor/Parsers.kt
@@ -2,15 +2,8 @@ package click.seichi.observerutils.contextualexecutor
 
 import click.seichi.observerutils.CommandBuildException
 import com.github.michaelbull.result.*
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 object Parsers {
-    fun formattedDate(vararg formatter: DateTimeFormatter, failureMessage: String): SingleArgumentParser = { str ->
-        formatter.firstNotNullOfOrNull { runCatching { LocalDate.parse(str, it) }.get() }
-            .toResultOr { CommandBuildException.FailedToParseArg(failureMessage) }
-    }
-
     fun listedInt(list: List<Int>, failureMessage: String): SingleArgumentParser = { str ->
         str.split(",").mapResult { parsedList ->
             parsedList.toIntOrNull()?.takeIf { list.contains(it) }
