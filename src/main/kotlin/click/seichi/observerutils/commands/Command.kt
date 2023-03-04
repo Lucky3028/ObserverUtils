8b8d2e2c538ab15c4466a74f69678a43a6129100
mel9y <me@mel9y.dev>
2023-03-04 18:45:41 +0900

    Revert "feat: surround region and mcid with atmark"
    This reverts commit 08d93395bd789d7f7c9395ff4d85f9bce5eed567.

--- src/main/kotlin/click/seichi/observerutils/commands/Command.kt
+++ src/main/kotlin/click/seichi/observerutils/commands/Command.kt
@@ -60,9 +60,9 @@ object Commands {
                     if (regions.size >= 2) "(${regions.size}): ${regions.joinToString { it.id }}" else "-"
                 val comment = context.args.yetToBeParsed.orEmpty("-") { it.joinToString("\n") }
                 val description = """
-                    |_.保護名|@@${topRegion.id}@@|
-                    |_.保護Owner|@@${topRegion.owners.uniqueIds.filterNotNull().formatted()}@@|
-                    |_.保護Member|@@${topRegion.members.uniqueIds.filterNotNull().formatted()}@@|
+                    |_.保護名|${topRegion.id}|
+                    |_.保護Owner|${topRegion.owners.uniqueIds.filterNotNull().formatted()}|
+                    |_.保護Member|${topRegion.members.uniqueIds.filterNotNull().formatted()}|
                     |_.重複保護|$duplicatedRegions|
                     |_.報告者コメント|$comment|
                 """.trimIndent()
