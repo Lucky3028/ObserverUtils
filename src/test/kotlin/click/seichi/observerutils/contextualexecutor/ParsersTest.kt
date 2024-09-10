0356a9d9b0b01e049968948c58c44a12e31295fb
Lucky3028 <yubi@clov3r.cc>
2024-09-11 08:23:29 +0900

    test: fix error with removed types

--- src/test/kotlin/click/seichi/observerutils/contextualexecutor/ParsersTest.kt
+++ src/test/kotlin/click/seichi/observerutils/contextualexecutor/ParsersTest.kt
@@ -2,14 +2,12 @@ package click.seichi.observerutils.contextualexecutor
 
 import click.seichi.observerutils.CommandBuildException
 import click.seichi.observerutils.contextualexecutor.Parsers.listedInt
-import com.github.michaelbull.result.Err
-import com.github.michaelbull.result.Ok
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.inspectors.forAll
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import kotlin.random.Random
-
-private typealias ParseError = Err<CommandBuildException>
 
 class ParsersTest : BehaviorSpec({
     Given("Parsers#listedIntで") {
@@ -17,24 +15,43 @@ class ParsersTest : BehaviorSpec({
         val failureMessage = ""
 
         When("空文字列ならば") {
-            Then("Error") { listedInt(base, failureMessage)("").shouldBeInstanceOf<ParseError>() }
+            Then("Error") {
+                val actual = listedInt(base, failureMessage)("")
+
+                actual.isErr.shouldBeTrue()
+                actual.error.shouldBeInstanceOf<CommandBuildException>()
+            }
         }
         When("splitできない文字列が含まれているならば") {
             Then("Error") {
                 setOf("a", "aiueo", "aiu123", "1/2/3", "1,2/aaaa")
-                    .forAll { listedInt(base, failureMessage)(it).shouldBeInstanceOf<ParseError>() }
+                    .forAll {
+                        val actual = listedInt(base, failureMessage)(it)
+
+                        actual.isErr.shouldBeTrue()
+                        actual.error.shouldBeInstanceOf<CommandBuildException>()
+                    }
             }
         }
         When("splitできるがIntでない文字列が含まれているならば") {
             Then("Error") {
                 setOf("a,i,u", "q,w,r", "aaa,iii,u", "qsaa,w,r,1", "1,2,a")
-                    .forAll { listedInt(base, failureMessage)(it).shouldBeInstanceOf<ParseError>() }
+                    .forAll {
+                        val actual = listedInt(base, failureMessage)(it)
+
+                        actual.isErr.shouldBeTrue()
+                        actual.error.shouldBeInstanceOf<CommandBuildException>()
+                    }
             }
         }
         When("splitもできるしIntのみの文字列であって") {
             When("基準に含まれているIntならば") {
                 Then("Ok") {
-                    listedInt(base, failureMessage)(base.joinToString(",")).shouldBeInstanceOf<Ok<List<String>>>()
+                    val actual = listedInt(base, failureMessage)(base.joinToString(","))
+
+                    actual.isOk.shouldBeTrue()
+                    actual.value.shouldBeInstanceOf<List<String>>()
+
                 }
             }
             When("基準に含まれていないIntが含まれているならば") {
@@ -42,7 +59,11 @@ class ParsersTest : BehaviorSpec({
                     (1..5).map {
                         (1..10).map { Random.nextInt() }.filterNot { base.contains(it) }.joinToString(",")
                     }.forAll {
-                        listedInt(base, failureMessage)(it).shouldBeInstanceOf<ParseError>()
+                        val actual = listedInt(base, failureMessage)(it)
+
+                        actual.isErr.shouldBeTrue()
+                        actual.error.shouldBeInstanceOf<CommandBuildException>()
+
                     }
                 }
             }
